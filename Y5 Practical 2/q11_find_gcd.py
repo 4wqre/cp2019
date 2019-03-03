d073ffb8a82ca00f27234cdb8fda066c0cca96fe
4wqre <47800389+4wqre@users.noreply.github.com>
2019-03-03 17:41:25 +0800

    Create q11_find_gcd.py

---
+++ Y5 Practical 2/q11_find_gcd.py
@@ -0,0 +1,15 @@
+def find_divisor(m,n):
+    while n in range (1, n):
+        if n % (m) == 0 and m % (m) == 0:
+            break
+        else:
+            m = m - 1
+    print("The greatest common divisor is " + str(m) + ".")
+    
+      
+
+n1 = int(input("Enter an integer: "))
+n2 = int(input("Enter another integer: "))
+
+if n1 < n2:
+    find_divisor(n1,n2)
