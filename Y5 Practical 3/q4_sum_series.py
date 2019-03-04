4390f7ec5f3da933cd477542172b16f32ce41a89
4wqre <47800389+4wqre@users.noreply.github.com>
2019-03-04 22:43:56 +0800

    Create q4_sum_series.py

---
+++ Y5 Practical 3/q4_sum_series.py
@@ -0,0 +1,23 @@
+#function m_series
+def m_series(i):
+    x = 1
+    n = 0
+    while x in range (1,i+1):
+        n = n + x/(x+1)
+        x = x + 1 
+    return n 
+
+#table
+n = 1
+table = {}
+header1 = "#i"
+header2 = "\t" + "m(i)"
+table[header1] = header2
+while n in range (0, 21):
+    num = "#" + str(n)
+    output = "\t" + "{0:.4f}".format(m_series(n))
+    table[num] = output
+    n = n + 1
+
+for key, value in table.items():
+    print(key, value)
